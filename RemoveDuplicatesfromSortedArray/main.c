7621ed2a305409c548345071af75511cd820d493
shuoz <shuoz@qq.com>
2017-07-08 16:13:24 +0800

    RemoveDuplicatesfromSortedArray

--- RemoveDuplicatesfromSortedArray/main.c
+++ RemoveDuplicatesfromSortedArray/main.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 #define bool int
+void removeOne(int *nums, int start, int n, int numsSize)
+{
+   int i;
+   for(i=start+1;i<=numsSize-n;i++)
+   {
+        nums[i] = nums[i+n-1];
+   }
+}
 int removeDuplicates(int* nums, int numsSize)
 {
+    
     int i;
-    for(i=0;i<numsSize-1;i++)
+    int j;
+    int k;
+    int returnSize = numsSize;
+    for(i=0;i<returnSize-1;i++)
     {
-        if(nums[i]==nums[j])
+        j = i+1;
+        while(nums[i]==nums[j] && j<numsSize)
         {
+            j++;
+            returnSize--;
         }
+        removeOne(nums, i, j-i, numsSize);
+        numsSize = returnSize;
+     }
+    for(i=0;i<returnSize;i++)
+    {
+        printf("%d\n", nums[i]);
     }
+    return numsSize;
 }
 int main() 
 {   
     int result;
-    int nums[5] = {1,3,3,3,1}; 
-    result = removeElement(nums, 5, 3);
+    int nums[5] = {1,1,1,1,1}; 
+    result = removeDuplicates(nums, 5);
     //printf("%d\n", result);
 } 
